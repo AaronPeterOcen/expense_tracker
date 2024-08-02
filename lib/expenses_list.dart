b1405bd075ad44b518b2a4bf31b04ea7517076aa
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-02 19:33:23 +0300

    rendering long lists with listView

---
+++ lib/expenses_list.dart
@@ -0,0 +1,28 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+// Expense
+
+// FlutterError
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expenses});
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a ListView using ListView.builder
+    return ListView.builder(
+      // Set the number of items in the list to the length of the expenses list
+      itemCount: expenses.length,
+      // Define how each item in the list should be built
+      itemBuilder: (ctx, index) {
+        // Create a Text widget to display the title of each expense
+        return Text(
+          expenses[index]
+              .title, // Access the title property of the expense at the current index
+        );
+      },
+    );
+  }
+}
