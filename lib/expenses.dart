e7d5934e56f8606ff4986b9e6a1af02d6ee09da7
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-02 18:11:13 +0300

    initial setup for the expense tracker app

---
+++ lib/expenses.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+// FlutterError
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text('Expenses Tracker'),
+          Text('initial Setup Tracker'),
+        ],
+      ),
+    );
+  }
+}
