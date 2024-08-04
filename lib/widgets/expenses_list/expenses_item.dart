02daed777cfaa4239bba4351eacd26e627ab29eb
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-04 06:13:13 +0300

    setting appbar with title and icons

--- lib/widgets/expenses_list/expenses_item.dart
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -1,5 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
-import 'package:expense_tracker/widgets/expenses.dart';
+// import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesItem extends StatelessWidget {
