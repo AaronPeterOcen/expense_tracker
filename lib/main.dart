885f0eb3fae3dc9fca03b9af7333113cb3b945e7
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-05 19:23:24 +0300

    using and setting color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,23 @@ void main() {
   runApp(const MyApp());
 }
 
+var lColorScheme =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 27, 237, 111));
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Expenses(),
+    return MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: lColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: lColorScheme.onPrimaryContainer,
+            foregroundColor: lColorScheme.onPrimary),
+      ),
+      home: const Expenses(),
     );
   }
 }
